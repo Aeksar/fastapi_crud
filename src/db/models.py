36afef362c07640e9d70bd4be73a9487ab1337ea
Aeksar <skoshkidko12345@mail.ru>
2025-08-28 19:53:19 +0300

    add user model

--- src/db/models.py
+++ src/db/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import String, Enum, func
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import String, Enum, Date, Boolean, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from uuid import UUID, uuid4
+from datetime import date
 
 from src.db.core import Base
-from src.utils.enums import TaskStatusEnum
+from src.utils.enums import TaskStatusEnum, UserRoleEnum
 
 
 class Task(Base):
@@ -17,4 +18,21 @@ class Task(Base):
         default=TaskStatusEnum.CREATED,
         nullable=False
     )
+    owner_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="tasks")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, index=True, default=uuid4)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    surname: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    birthdate: Mapped[date] = mapped_column(Date(), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
+    role: Mapped[Enum] = mapped_column(Enum(UserRoleEnum), default=UserRoleEnum.USER, nullable=False)
+
+    tasks = relationship("Task", back_populates="owner")
 
--- src/utils/enums.py
+++ src/utils/enums.py
@@ -4,3 +4,9 @@ class TaskStatusEnum(str, Enum):
     CREATED = "создано"
     IN_WORK = "в работе"
     COMLETED = "завершено"
+
+class UserRoleEnum(str, Enum):
+    USER = "Пользователь"
+    REDACTOR = "Редактор"
+    ADMIN = "Администратор"
+    

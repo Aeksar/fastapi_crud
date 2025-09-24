58ecf2c390ae0a5ff75a4c4b9313297b70409b59
Aeksar <skoshkidko12345@mail.ru>
2025-09-24 18:11:04 +0300

    add create_rokens

--- src/auth/create.py
+++ src/auth/create.py
@@ -46,5 +46,17 @@ def create_verification_token(user: UserResponse):
         expire_minutes=settings.auth.verification_token_expire_minutes
     )
 
+def create_tokens(user: UserResponse, response: Response):
+    """
+    Create access and refresh tokens to the response cookies.
 
-
+    Args:
+        user (UserResponse): User data.
+        response (Response): Response object to set cookies.
+    Returns:
+        TokenInfo: Access and refresh tokens.
+    """
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+    set_tokens_to_cookie(response, access_token, refresh_token)
+    return TokenInfo(access_token=access_token, refresh_token=refresh_token)
